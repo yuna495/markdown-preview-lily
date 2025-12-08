5bf716b212fb40128c1250a4b1e234b435386669
yuna495 <aa49569@Gmail.com>
2025-12-08 21:42:51 +0900

    mrakmap

--- src/extension.js
+++ src/extension.js
@@ -1,22 +1,33 @@
 // The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// 作成予定のMarkmapプラグインをインポートします
+const markmapPlugin = require('./markmap-it-plugin');
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	// 拡張機能が有効になったことをコンソールに出力します
 	console.log('Markdown Preview Plus is now active!');
 }
 
-// This method is called when your extension is deactivated
+// この関数をエクスポートすることで、VS CodeのMarkdown拡張機能が
+// 独自のマークダウン-itインスタンスを拡張するために呼び出します。
+/**
+ * @param {import('markdown-it')} md
+ * @returns {import('markdown-it')}
+ */
+function extendMarkdownIt(md) {
+    // Markmapプラグインを適用
+    // ここに将来Mermaidプラグインも追加できます
+    return md.use(markmapPlugin);
+}
+
+
 function deactivate() {}
 
 module.exports = {
 	activate,
+    extendMarkdownIt,
 	deactivate
 }
